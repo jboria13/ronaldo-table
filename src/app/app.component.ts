e0bc6c43db5807a4d96d0cf539fdfce57c5cb2fc
Joe Boria <joeboria@gmail.com>
2022-10-12 15:20:50 -0500

    removed unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { privateDecrypt } from 'crypto';
 import { Competition } from 'src/models/competitions';
 import { People } from 'src/models/people';
 import { tableOfStats } from 'src/models/ronaldoDomesticTable';
